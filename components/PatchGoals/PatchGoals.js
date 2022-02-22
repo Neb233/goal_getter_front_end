061003ca1b845de0779a37347a120791a5816e0d
fynnmeredith <91606637+fynnmeredith@users.noreply.github.com>
2022-02-22 16:46:47 +0000

    update display of components for feed

--- components/PatchGoals/PatchGoals.js
+++ components/PatchGoals/PatchGoals.js
@@ -2,24 +2,30 @@ import React from "react";
 import { StyleSheet, View, Text, TextInput, ScrollView } from "react-native";
 import { FlatList } from "react-native-web";
 import Goals from "../Goals/Goals";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 const PatchGoals = () => {
+    const [goals, setGoals] = useState([]);
+
+     useEffect(() => {});
+
   return (
     <View style={styles.cont}>
       <Text style={styles.text}>Current goals:</Text>
-{/* pagingEnabled={true} */}
-      <ScrollView horizontal={true} >
+      <ScrollView horizontal={true} pagingEnabled={true}>
         <View style={styles.page}>
-            <Text style={styles.pageText}>Goal 1</Text>
+          <Text style={styles.pageText}>Goal 1</Text>
         </View>
         <View style={styles.page}>
-        <Text style={styles.pageText}>Goal 2</Text>
+          <Text style={styles.pageText}>Goal 2</Text>
         </View>
         <View style={styles.page}>
-        <Text style={styles.pageText}>Goal 3</Text>
+          <Text style={styles.pageText}>Goal 3</Text>
         </View>
         <View style={styles.page}>
-        <Text style={styles.pageText}>Goal 4</Text>
+          <Text style={styles.pageText}>Goal 4</Text>
         </View>
       </ScrollView>
     </View>
@@ -49,20 +55,20 @@ const styles = StyleSheet.create({
   text: {
     padding: 10,
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   page: {
     height: 340,
     width: 335,
     marginLeft: 5,
     marginRight: 5,
-    backgroundColor: "#afca51",
-    borderRadius: 5
+    backgroundColor: "#abbabe",
+    borderRadius: 5,
   },
   pageText: {
     padding: 10,
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white'
-  }
+    fontWeight: "bold",
+    color: "white",
+  },
 });
--- components/Social/Social.js
+++ components/Social/Social.js
@@ -1,29 +1,56 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import * as Progress from "react-native-progress";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { TouchableOpacity } from "react-native";
+import PostComments from "../PostComments/PostComments";
+import { useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
 
 const Social = (props) => {
+  const [isShowing, setIsShowing] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const handleCommentClick = () => {
+    setIsShowing((currValue) => {
+      return !currValue;
+    });
+  };
+
   return (
-    <View style={styles.goalContainer}>
-      <Text style={styles.goalInfo}>{props.text}</Text>
-      <Progress.Bar
-        style={styles.progress}
-        progress={props.progress}
-        width={300}
-      />
-      <View style={styles.interact}>
-        <TouchableOpacity>
-          <Text style={styles.descrip}>Congratulate</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.update}>Comment</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.update}>Details</Text>
-        </TouchableOpacity>
+    <KeyboardAvoidingView>
+      <View style={styles.goalContainer}>
+        <View style={styles.userInfo}>
+          <View style={styles.profilePic} />
+          <Text style={styles.goalInfo}>{props.text}</Text>
+        </View>
+        <View style={styles.post}>
+          <Text>"{props.upload}"</Text>
+        </View>
+        <View style={styles.interact}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.congratulate}>Congratulate</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.comButton}
+            onPress={() => {
+              handleCommentClick();
+            }}
+          >
+            <Text style={styles.comment}>Comment</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          {isShowing ? (
+            <TextInput
+              style={styles.input}
+              placeholder="leave a positive comment"
+              onChangeText={(comment) => setComments(comment)}
+              defaultValue={comments}
+            />
+          ) : null}
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -54,28 +81,55 @@ const styles = StyleSheet.create({
     color: "black",
     marginBottom: 15,
     fontWeight: "bold",
+    margin: 10,
   },
   progress: {
     marginTop: 10,
   },
   interact: {
     flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    paddingTop: 15,
+    borderRadius: 10,
   },
-  descrip: {
-    padding: 4,
-    backgroundColor: "white",
+  profilePic: {
+    height: 40,
+    width: 40,
+    backgroundColor: "grey",
+    borderRadius: 50,
+  },
+  userInfo: {
+    flexDirection: "row",
+  },
+  post: {
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 10,
   },
-  update: {
-    padding: 4,
-
+  congratulate: {
+    color: "white",
+    padding: 2,
+  },
+  button: {
+    backgroundColor: "#468705",
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  comButton: {
+    backgroundColor: "#4892b7",
+    borderRadius: 8,
+    marginTop: 20,
+    marginLeft: 10,
+    padding: 2,
+  },
+  comment: {
+    color: "white",
+  },
+  input: {
+    padding: 7,
+    marginTop: 15,
     backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 50,
   },
 });
 
