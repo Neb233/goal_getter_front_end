0573b949497575a6811ab6ef466425bf8ff6b800
Neb233 <bartram.benjamin@gmail.com>
2022-02-23 16:14:47 +0000

    add ability to set start and end date for subgoals

--- components/Set_Goal/SubGoalForm.js
+++ components/Set_Goal/SubGoalForm.js
@@ -2,19 +2,27 @@ import React from "react";
 import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 import { Formik } from "formik";
 import * as yup from "yup";
+import DatePicker from "../../shared/DatePicker";
+import { prodErrorMap } from "firebase/auth";
+
 
 const ReviewSchema = yup.object({
   title: yup.string().required().min(4),
   objective: yup.string().required().min(10),
+  start_date: yup.date().required(),
+  end_date: yup.date().required()
 });
 
 const SubGoalForm = ({ addSubGoal }) => {
   return (
+    
     <View>
       <Formik
         initialValues={{
           title: "",
           objective: "",
+          start_date: "",
+          end_date: ""
         }}
         onSubmit={(values) => {
           addSubGoal(values);
@@ -22,17 +30,26 @@ const SubGoalForm = ({ addSubGoal }) => {
         validationSchema={ReviewSchema}
       >
         {(props) => (
-          <View style={styles.container}>
+          <View>
+           
+            <View style={styles.container}>
+            
             <TextInput
               style={styles.input}
+              multiline
+              
               placeholder="SubGoal Title"
               onChangeText={props.handleChange("title")}
               value={props.values.title}
             />
+             
+            </View>
             <Text style={styles.errorText}>
               {props.touched.title && props.errors.title}
             </Text>
-
+            
+            
+            <View style={styles.container}>
             <TextInput
               style={styles.input}
               multiline
@@ -40,33 +57,41 @@ const SubGoalForm = ({ addSubGoal }) => {
               onChangeText={props.handleChange("objective")}
               value={props.values.objective}
             />
+           
+            </View>
             <Text style={styles.errorText}>
-              {props.touched.objective && props.errors.objective}
+              {props.touched.objective && props.errors.end_date}
             </Text>
 
+
+            <DatePicker 
+            name="start_date"/>
+            <Text style={styles.errorText}>
+              {props.touched.start_date && props.errors.start_date}
+            </Text>
+
+            <DatePicker
+             name="end_date" />
+
             <Button
-              title="submit"
+              title="Add SubGoal"
               color="maroon"
               onPress={props.handleSubmit}
             />
           </View>
         )}
       </Formik>
+
+     
     </View>
+   
+ 
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    borderColor: "#ddd",
-    padding: 10,
-    margin: 5,
-    fontSize: 18,
-    borderRadius: 6,
-  },
   container: {
-    flex: 1,
+    flexDirection: "row",
     padding: 20,
   },
   errorText: {
@@ -75,6 +100,11 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 6,
   },
+  input: {
+    
+borderColor: "black",
+borderRadius:10
+  }
 });
 
 export default SubGoalForm;
