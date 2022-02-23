4c7521eeab06f6590b7692d910a347ccec2a70e3
AB <andrewbrowne2@gmail.com>
2022-02-23 08:52:50 +0000

    add uplaod photo function

--- components/Login/RegisterScreen.js
+++ components/Login/RegisterScreen.js
@@ -1,22 +1,65 @@
 import { TouchableOpacity,
-    KeyboardAvoidingView, StyleSheet, Text, View, TextInput } from 'react-native'
+    KeyboardAvoidingView, StyleSheet, Text, View, TextInput, Button, Image } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import { auth } from '../../firebase'
-import { createUserWithEmailAndPassword,  onAuthStateChanged } from '@firebase/auth';
+import { createUserWithEmailAndPassword,  onAuthStateChanged, sendSignInLinkToEmail } from '@firebase/auth';
 import {  useNavigation } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker'
+import {getStorage, ref, uploadBytes} from 'firebase/storage'
 
 
 import {  Formik} from 'formik'
 import * as Yup from 'yup'
 
 
+
+
+
 const RegisterScreen = () => {
+
+    const [image, setImage] = useState(null)
+    const [id, setId] = useState('')
+   
+    
+
+  const pickImage = async () => {
+ 
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+
+
+      setImage(result.uri);
+      const storage = getStorage();
+      const refo = ref(storage, `${id}`)
+
+      const img = await fetch(result.uri);
+    const bytes = await img.blob();
+
+        await uploadBytes(refo, bytes)
+
+
+    }
+  };
+
+
+
+
+
 
  const valSchema = Yup.object({
 
         username: Yup.string()
         .min(2, 'Must be at least 2 characters')
-        .max(8, 'no more than 8 characters'),
+        .max(8, 'no more than 8 characters')
+        .required('Required'),
         email: Yup.string()
         .email('Invalid email address')
         .required('Required'),
@@ -32,7 +75,9 @@ const RegisterScreen = () => {
   
 
       return (
+          <View style={styles.inputContainer}>
         <Formik  initialValues={{firstName: '',
+        username: '',
         password: '',
         email: ''}}
         validationSchema={valSchema}
@@ -40,12 +85,12 @@ const RegisterScreen = () => {
             createUserWithEmailAndPassword(auth, values.email, values.password)
             .then(userCredentials => {
                 const user = userCredentials.user;
-                console.log(values.email)
-            
-                
-            })
+                user.photoURL = image
+                console.log(user)
+             })
            
             .catch(error => alert(error.message))
+            
      }
      
         >
@@ -56,13 +101,14 @@ const RegisterScreen = () => {
 
 
           
-            <View style={styles.inputContainer}>
+            <View >
                 <TextInput
                 id="email"
                 name="email"
                 placeholder='email'
                 value={values.email}
                 onChange={handleChange('email')}
+            
                 style={styles.input}
                 />
                 <Text>
@@ -120,6 +166,23 @@ const RegisterScreen = () => {
         </KeyboardAvoidingView>
              )}
         </Formik>
+
+        <View >
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <TextInput placeholder='give image a title'
+       onChangeText={text=> setId(text)}
+    style={styles.buttonOutline}
+    value={id} />
+     
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+
+
+       
+        </View>
+
+
+
        )
      }
      
