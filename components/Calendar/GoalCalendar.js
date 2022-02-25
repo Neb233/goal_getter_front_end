c9549c376b115a773f542126fbc1306c619d9828
fynnmeredith <91606637+fynnmeredith@users.noreply.github.com>
2022-02-25 16:08:16 +0000

    calendar goals update

--- components/Calendar/GoalCalendar.js
+++ components/Calendar/GoalCalendar.js
@@ -3,48 +3,60 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Agenda } from "react-native-calendars";
 import { useState, useEffect } from "react";
 import { Card } from "react-native-paper";
-import { getSubGoalsByUser } from "../../utils/api";
-
-// const dateToString = (time) => {
-//   const date = new Date(time);
-//   return date.toISOString().split("T")[0];
-// };
-
+import { getGoalsByUser, getSubGoalsByUser } from "../../utils/api";
+import {
+  formatGoalsForCalendar,
+  formatSubgoalsForCalendar,
+} from "../../utils/FormatDates";
 
 const GoalCalendar = () => {
-  const [items, setItems] = useState({'2022-02-25': [{name: 'objective'}]})
+  const [items, setItems] = useState({});
 
-  const [subgoals, setSubgoals] = useState({})
-
-  const subgoalsArray = [{...subgoals}]
-  const itemFormat = subgoalsArray.map((subgoal, index) => {
-    
-  })
-  
+  const [goals, setGoals] = useState({});
 
   useEffect(() => {
-    getSubGoalsByUser('jeff').then((res) => {
-      setSubgoals(res)
-    })
-  }, ['jeff']);
-  
-const renderItem = (items) => {
-  return (
-    <View style={{margin: 50}}>
-      <Text>{items.name}</Text>
-    </View>
-  )
-}
+    getSubGoalsByUser("jeff")
+      .then((subgoals) => {
+        const formattedSubgoals = formatSubgoalsForCalendar(subgoals);
+        setItems(formattedSubgoals);
+        getGoalsByUser("jeff");
+        // console.warn(formattedSubgoals)
+      })
+      .then((goals) => {
+        const formattedGoals = formatGoalsForCalendar(goals);
+        setItems((formattedSubgoals) => {
+          const combinedGoalsObject = { ...formattedSubgoals };
+          const formattedGoalsDates = Object.keys(formattedGoals);
+          formattedGoalsDates.forEach((date) => {
+            if (combinedGoalsObject.hasOwnProperty(date)) {
+              combinedGoalsObject[date].push(...formattedGoals[date]);
+            } else {
+              combinedGoalsObject[date] = formattedGoals[date];
+            }
+          });
+          return combinedGoalsObject;
+        });
+      });
+  }, []);
+
+  const renderItem = (items) => {
+    return (
+      <View style={{ margin: 50 }}>
+        <Text>{items.name}</Text>
+      </View>
+    );
+  };
+
+  // const renderGoal = (goals) => {
+  //   <View style={{ margin: 50 }}>
+  //       <Text>{goals.name}</Text>
+  //     </View>
+  // }
   return (
     <View style={{ flex: 1, marginTop: 0 }}>
-
-      <Agenda
-      selected={"2022-02-25"}
-      items={items}
-      renderItem={renderItem}/>
+      <Agenda selected={"2022-02-25"} items={items} renderItem={renderItem} />
     </View>
   );
-
 };
 
 export default GoalCalendar;
